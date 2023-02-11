d9ec1f8ffcb723268f8a069606c7022ebe393cf9
ekshanya <110779995+ekshanya@users.noreply.github.com>
2023-02-11 17:32:41 +0530

    Update Supplier.sol

--- Supplier.sol
+++ Supplier.sol
@@ -1,99 +1,104 @@
 pragma solidity 0.4.4;
-// call the Tracking contract at its deployed address
-// need to include Tracking contract code at the end
-Tracking track = Tracking(0x0dcd2f752394c41875e259e00bb44fd505297caf);
-address admin;
-mapping (address => Supplier) suppliers;
-address[] suppliersByAddress; // array of all suppliers' accounts
-struct Supplier {
-string name;
-uint phoneNo;
-string cityState;
-string country;
-string goodsType;
-uint reputation;
-}
-// constructor - runs once when contract is deployed
-function Reputation() {
-admin = msg.sender;
-}
-// modifier to allow only admin to execute a function
-modifier onlyAdmin() {
-if (msg.sender != admin) throw;
-_;
-}
-// function for supplier to add their details to database
-function addSupplier(string _name, uint _phoneNo, string
-_cityState, string _country, string _goodsType) returns (bool success)
+import "./Track.sol";
+contract Reputation
 {
-// don't overwrite existing entries and ensure name isn't null
-if (bytes(suppliers[msg.sender].name).length == 0 &&
-bytes(_name).length != 0) {
-suppliers[msg.sender].name = _name;
-suppliers[msg.sender].phoneNo = _phoneNo;
-suppliers[msg.sender].cityState = _cityState;
-suppliers[msg.sender].country = _country;
-suppliers[msg.sender].goodsType = _goodsType;
-suppliers [msg. sender] .reputation = track.calculateReputation(msg.sender);
-suppliersByAddress.push(msg.sender);
-return true;
+    Tracking track = Tracking(0xaE036c65C649172b43ef7156b009c6221B596B8b);
+    address admin;
+    mapping (address => Supplier) suppliers;
+    address[] suppliersByAddress; 
+    struct Supplier {
+        string name;
+        uint phoneNo;
+        string cityState;
+        string country;
+        string goodsType;
+        uint reputation;
+    }
+    function Reputation() {
+        admin = msg.sender;
+    }
+    modifier onlyAdmin() {
+        if (msg.sender != admin) throw;
+        _;
+    }
+
+    function addSupplier(string _name, uint _phoneNo, string _cityState, string _country, string _goodsType) returns (bool success)
+    {
+    if (bytes(suppliers[msg.sender].name).length == 0 && bytes(_name).length != 0) {
+        suppliers[msg.sender].name = _name;
+        suppliers[msg.sender].phoneNo = _phoneNo;
+        suppliers[msg.sender].cityState = _cityState;
+        suppliers[msg.sender].country = _country;
+        suppliers[msg.sender].goodsType = _goodsType;
+        suppliers [msg. sender] .reputation = track.calculateReputation(msg.sender);
+        suppliersByAddress.push(msg.sender);
+        return true;
+    }
+    else {
+        return false; 
+    }
+    }
+    function removeSupplier(address _supplier) onlyAdmin returns (bool success) 
+    {
+        delete suppliers[_supplier];
+        for (uint i = 0; i < suppliersByAddress.length; i++) 
+        {
+            if (suppliersByAddress[i] == _supplier)
+             {
+                for (uint index = i; index < suppliersByAddress.length- 1; index++) 
+                {
+                    suppliersByAddress[index] =suppliersByAddress[index + 1];
+                }
+                delete suppliersByAddress[suppliersByAddress.length-1];
+            }
+                suppliersByAddress.length--;
+        }
+    
+    return true;
 }
-else {
-return false; // either entry already exists or name entered was null
-}
-}
-// function to remove supplier from database (can only be done byadmin)
-function removeSupplier(address _supplier) onlyAdmin returns (bool success) {
-delete suppliers[_supplier];
-// delete entry from array and shorten array
-for (uint i = 0; i < suppliersByAddress.length; i++) {
-if (suppliersByAddress[i] == _supplier) {
-for (uint index = i; index < suppliersByAddress.length- 1; index++) {
-    suppliersByAddress[index] =suppliersByAddress[index + 1];
-}
-delete suppliersByAddress[suppliersByAddress.length-1];}
-suppliersByAddress.length--;
-}
-}
-return true;
-// function to di
-function findSupplier(address _supplier) constant returns (string, uint, string, string, string,uint)
-{
-    return (suppliers[_supplier].name,suppliers[_supplier]._phoneNo,suppliers[_supplier].cityState,suppliers [_supplier].country,suppliers [_supplier]goodsType,suppliers [_supplier].reputation);
-// function to display all suppliers' accounts in database
-function allSuppliers() constant returns (address[]) {
-return suppliersByAddress;
-}
-function filterByGoodsType(string _goodsType) constant returns (address[]) {
-address[] memory filteredGoods = new address [ (suppliersByAddress. length);
-for (uint i = 0; i < suppliersByAddress.length; i++) {
-if (sha3(suppliers[suppliersByAddress[i]].goodsType) == sha3(_goodsType)) {
-filteredGoods[i] = suppliersByAddress[i];
-}
-}
-return filteredGoods;
-}
-// function to search for suppliers by reputation (returns thosen with same or higher reputation)
-function filterByReputation(uint _reputation) constant returns (address[]) {
-address[] memory filteredRep = new address [] (suppliersByAddress. length);
-for (uint i = 0; i < suppliersByAddress.length; i++) {
-if (suppliers[suppliersByAddress[i]].reputation >= _reputation) {
-filteredRep[i] = suppliersByAddress[i];
-}
-}
-return filteredRep;
-}
-// function to display reputation of supplier (calls Tracking contract)
-function checkReputation(address _supplier) constant returns(uint) {
-return track.calculateReputation(_supplier);
-}
-// function to update reputations of all suppliers (calls Tracking
-contract)
-// can only be done by admin
-function updateReputations() onlyAdmin returns (bool success) {
-for (uint i = 0; i < suppliersByAddress.length; i++) {
-suppliers[suppliersByAddress[i]].reputation = track.calculateReputation(suppliersByAddress[i]);
-}
-return true;
-}
+
+    function findSupplier(address _supplier) constant returns (string, uint, string, string, string,uint)
+    {
+    return (suppliers[_supplier].name,suppliers[_supplier].phoneNo,suppliers[_supplier].cityState,suppliers [_supplier].country,suppliers [_supplier].goodsType,suppliers [_supplier].reputation);
+    }
+    function allSuppliers() constant returns (address[]) {
+        return suppliersByAddress;
+    }
+    function filterByGoodsType(string _goodsType) constant returns (address[])
+     {
+        address[] memory filteredGoods = new address [](suppliersByAddress.length);
+        for (uint i = 0; i < suppliersByAddress.length; i++) 
+        {
+            if (sha3(suppliers[suppliersByAddress[i]].goodsType) == sha3(_goodsType))
+             {
+                filteredGoods[i] = suppliersByAddress[i];
+            }
+        }
+    return filteredGoods;
+    }
+
+    function filterByReputation(uint _reputation) constant returns (address[]) {
+        address[] memory filteredRep = new address [] (suppliersByAddress. length);
+            for (uint i = 0; i < suppliersByAddress.length; i++) 
+            {
+                if (suppliers[suppliersByAddress[i]].reputation >= _reputation) 
+                {
+                    filteredRep[i] = suppliersByAddress[i];
+                }
+            }
+        return filteredRep;
+    }
+
+    function checkReputation(address _supplier) constant returns(uint) {
+        return track.calculateReputation(_supplier);
+    }
+
+    function updateReputations() onlyAdmin returns (bool success) {
+        for (uint i = 0; i < suppliersByAddress.length; i++) 
+        {
+            suppliers[suppliersByAddress[i]].reputation = track.calculateReputation(suppliersByAddress[i]);
+        }
+        return true;
+    }
+
 }
